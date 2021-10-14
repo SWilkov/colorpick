953a4d319484673d2be12eac5a24c050db1ae5c3
Scott Williamson <scott.williamson@odds-bods.com>
2021-10-14 17:13:43 +0100

    added hexadecimal selectors for components

---
+++ src/app/selectors/hex.selector.ts
@@ -0,0 +1,40 @@
+import { createSelector } from "@ngrx/store";
+import { AppState } from "../reducers";
+import * as fromHex from '../reducers/hex.reducer';
+
+const getHexadecimalState = (state: AppState) => state.hexadecimal;
+
+export const selectValidating = createSelector(
+  getHexadecimalState,
+  fromHex.getValidating
+);
+
+export const selectValidated = createSelector(
+  getHexadecimalState,
+  fromHex.getValidated
+);
+
+export const selectOriginalInput = createSelector(
+  getHexadecimalState,
+  fromHex.getOriginalInput
+);
+
+export const selectCalculating = createSelector(
+  getHexadecimalState,
+  fromHex.getCalculating
+);
+
+export const selectCalculated = createSelector(
+  getHexadecimalState,
+  fromHex.getCalculated
+);
+
+export const selectRgbType = createSelector(
+  getHexadecimalState,
+  fromHex.getRgbType
+);
+
+export const selectRgbValue = createSelector(
+  getHexadecimalState,
+  fromHex.getRgbValue
+);
