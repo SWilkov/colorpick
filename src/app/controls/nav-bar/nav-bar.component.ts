c8bd67c0e72c70f0de7e380740db90c745dfe3b1
Scott Williamson <scott.williamson@odds-bods.com>
2021-10-16 07:52:22 +0100

    update navbar with color change

--- src/app/controls/nav-bar/nav-bar.component.ts
+++ src/app/controls/nav-bar/nav-bar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
+import { Rgba } from 'src/app/models/rgba.model';
+import { AppState } from 'src/app/reducers';
+import * as hexSelectors from '../../selectors/hex.selector';
+import * as imageSelectors from '../../selectors/image.selector';
 
 @Component({
   selector: 'cp-nav-bar',
@@ -6,10 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  rgba$: Observable<Rgba>;
+  opacity$: Observable<number>;
+  rgbaSubscription: Subscription;
+  
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.rgba$ = this.store.select(hexSelectors.selectRgba);
+    this.opacity$ = this.store.select(imageSelectors.selectOpacity);
   }
 
+  getFriendlyRgba = (rgba: Rgba): string =>
+    `rgba(${rgba.red}, ${rgba.green}, ${rgba.blue}, ${rgba.alpha})`;
 }
