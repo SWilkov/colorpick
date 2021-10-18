7def5e2a149be8ccdb0e0d1f8de0ae9add8b645f
Scott Williamson <scott.williamson@odds-bods.com>
2021-10-18 09:58:10 +0100

    creating new hex to rgba page

---
+++ src/app/pages/hex-to-rgba/hex-to-rgba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cp-hex-to-rgba',
+  templateUrl: './hex-to-rgba.component.html',
+  styleUrls: ['./hex-to-rgba.component.scss']
+})
+export class HexToRgbaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
