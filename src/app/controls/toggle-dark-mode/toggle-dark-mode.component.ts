4cf351d9af4d99b051be9ac4a9704cdcc273f2c0
Scott Williamson <scott.williamson@odds-bods.com>
2021-10-27 19:06:35 +0100

    toggle btn should begin in dark mode

--- src/app/controls/toggle-dark-mode/toggle-dark-mode.component.ts
+++ src/app/controls/toggle-dark-mode/toggle-dark-mode.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Theme } from 'src/app/models/theme.enum';
 import { AppState } from 'src/app/reducers';
 import * as themeActions from '../../actions/theme.actions';
-
+import * as themeSelectors from '../../selectors/theme.selector';
 @Component({
   selector: 'cp-toggle-dark-mode',
   templateUrl: './toggle-dark-mode.component.html',
   styleUrls: ['./toggle-dark-mode.component.scss']
 })
 export class ToggleDarkModeComponent implements OnInit {
+  theme$: Observable<Theme>;
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.theme$ = this.store.select(themeSelectors.selectTheme);
   }
 
   toggle(): void {
